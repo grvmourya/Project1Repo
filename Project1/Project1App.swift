46b7963a28027d621402a333a847be1c34384ed2
grvmourya <gauravmourya1999@gmail.com>
2024-10-25 15:49:07 +0530

    Initial Commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Gaurav Mourya on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
